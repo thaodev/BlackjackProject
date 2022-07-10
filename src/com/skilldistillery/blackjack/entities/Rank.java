367723911a361d35cbd0070fff9a70e53432ac80
Thao Nguyen <thaontt1408@gmail.com>
2022-07-10 16:10:05 -0600

    Add second value for enum fields to declare the second valud of ACE

--- src/com/skilldistillery/blackjack/entities/Rank.java
+++ src/com/skilldistillery/blackjack/entities/Rank.java
@@ -1,20 +1,24 @@
 package com.skilldistillery.blackjack.entities;
 
 public enum Rank {
-	TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
-	TEN(10), JACK(10), QUEEN(10), KING(10),
-	ACE(11);
+	TWO(2, 2), THREE(3, 3), FOUR(4, 4), FIVE(5, 5), SIX(6, 6), SEVEN(7, 7), EIGHT(8, 8), NINE(9, 9),
+	TEN(10, 10), JACK(10, 10), QUEEN(10, 10), KING(10, 10),
+	ACE(11, 1);
 
 	private int value;
+	private int secondValue;
 	
-	Rank(int value){
+	Rank(int value, int secondValue){
 		this.value = value;
+		this.secondValue = secondValue;
 	}
 	
-
-
 	public int getValue() {
 		return value;
+	}
+	
+	public int getSecondValue() {
+		return secondValue;
 	}
 	
 }
