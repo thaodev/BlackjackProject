37feae46273876cec9682738d2657512f9e189fc
Thao Nguyen <thaontt1408@gmail.com>
2022-07-10 17:15:23 -0600

    Add Ranks for Cards in hand method in to support in boolean isBlackJack method

--- src/com/skilldistillery/blackjack/entities/BlackjackHand.java
+++ src/com/skilldistillery/blackjack/entities/BlackjackHand.java
@@ -24,13 +24,13 @@ public class BlackjackHand extends Hand{
 	
 	public boolean isBlackjack() {
 		boolean isHandBlackjack = true;
-		for (Card card : cards) {
-		if (card.getRank() == Rank.ACE && getHandValue() == 21) {
+		List<Rank> listOfRank = getRankInCards(cards);
+		if (cards.size() == 2 && listOfRank.contains(Rank.ACE)  && getHandValue() == 21 ) {
 			isHandBlackjack = true;
 		} else {
 			isHandBlackjack =  false;
 		}
-		}
+		
 		return isHandBlackjack;
 		
 	}
@@ -42,6 +42,14 @@ public class BlackjackHand extends Hand{
 			return false;
 		}
 		
+	}
+	
+	public List<Rank> getRankInCards(List<Card> cards){
+		List<Rank> listOfRank = new ArrayList<>();
+		for (Card card : cards) {
+			listOfRank.add(card.getRank());
+		}
+		return listOfRank;
 	}
 
 }
